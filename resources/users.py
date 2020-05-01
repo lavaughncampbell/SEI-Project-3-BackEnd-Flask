77042e0796eda1668992a8ac6b3472d34f0cb7ef
Mac <mac@Macs-MacBook.local>
2020-05-01 13:12:01 -0500

    can see JSON by sending it to POST /api/v1/users/register

--- resources/users.py
+++ resources/users.py
@@ -3,7 +3,7 @@
 
 import models # get all the models. specifically user model
 
-from flask import Blueprint # Blueprint is how we make our controllers
+from flask import Blueprint, request # Blueprint is how we make our controllers
 
 # make this a blueprint
 users = Blueprint('users', 'users')
@@ -11,3 +11,15 @@ users = Blueprint('users', 'users')
 @users.route('/', methods=['GET'])
 def test_user_resource():
   return "user resource works"
+
+# registration will be POST because were sending
+# req.body there is going to be JSON
+@users.route('/register', methods=['POST'])
+def register():
+  # this step is similar to making sure we can log
+  # req.body in express.
+  # note: we had to send JSON from postman(choose raw, select
+  # JSON from the drop down menu, and type a perfect JSON object with
+  # double qoutes around the keys
+  print(request.get_json())
+  return "check terminal"
