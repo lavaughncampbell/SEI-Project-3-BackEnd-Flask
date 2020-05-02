38a1c85dad570788412f86396b3be50e139f4307
Mac <mac@Macs-MacBook.local>
2020-05-02 17:57:51 -0500

    create route working, index route in progress

--- resources/posts.py
+++ resources/posts.py
@@ -4,7 +4,16 @@ import models
 # a blueprint is a way to create a self-contained grouping
 # of related functionalities in an app
 # we will use Blueprints to create basically a container
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
+# request - data from client's request is send to the global request object
+# we can use this object to get the json or form data or whatever is in the request
+# the request global var will be reassigned every time a request comes in that
+# has a body (POST, PUT)
+
+# this is some useful tools that come with peewee
+from playhouse.shortcuts import model_to_dict
+
+
 
 # BLUEPRINT
 
@@ -19,10 +28,39 @@ posts = Blueprint('posts', 'posts')
 # POST INDEX ROUTE
 @posts.route('/', methods=['GET'])
 def posts_index():
-  return "posts resource working"
+  results = models.Dog.select()
+
+  print('result of dog select query')
+  print(result)
+
+  for row in result:
+    print(row)
+
+  return "check your terminal"
+
+
+
+
+
+
 
 # POST CREATE ROUTE
 # note: for this route you need a trailing slash (i.e. /)
 @posts.route('/', methods=['POST'])
-def create_dog():
-  return "you hit dog create route"
+def create_post():
+  # .get_json() attache to request will extract JSON from req.body
+  payload= request.get_json() # this is like req.body in express!!!
+  print(payload) # you should see request body in your terminal
+  new_post = models.Post.create(description=payload['description'], user=payload['user'], comment=payload['comment'])
+
+  print(new_post) # just prints the ID -- check sqlite3 to see the data
+                  # run sqlite3 dogs.sqlite and run SQL queries in the CLI
+
+  # we can use model_to_dict from playhouse
+  post_dict = model_to_dict(new_post)
+
+  return jsonify(
+    data=dog_dict,
+    message="Successfully created post!",
+    status=201
+  ), 201
